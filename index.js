49e99b4f8e6268be5411c379bffaed672ce5b129
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 14:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name){
-var newKittens = ["Milo","Otis","Garfield",name]
-return newKittens}
+return kittens.push(name)
+  
+}
